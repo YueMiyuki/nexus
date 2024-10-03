4a36a02d8fc8fb7c9f0e5c5962f6c19ddb9ffe0d
ItzMiracleOwO <miraclecutelemon@gmail.com>
2024-10-03 00:51:34 +0000

    fix stupidness

--- app/robots.ts
+++ app/robots.ts
@@ -5,10 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: '/',
-      },
-      {
-        userAgent: '*',
         allow: ['/', '/linktree'],
       },
     ],
